029b1398db73b03d946c3b78f5f2b60a9ec5104c
Ed Cragg <ed.cragg@eipi.xyz>
2021-10-18 12:26:33 +0000

    Initial laptop configs

---
+++ dinky/system-packages.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+
+  environment.systemPackages = with pkgs; [
+      file
+      firefox
+      git
+      platformio
+      solvespace
+      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      wget
+  ];
+}
