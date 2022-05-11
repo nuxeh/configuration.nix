ce02cbcf216bc58f39075164c1dcab10652f9156
Ed Cragg <ed.cragg@eipi.xyz>
2022-05-11 13:51:54 +0100

    Package updates

--- dinky/system-packages.nix
+++ dinky/system-packages.nix
@@ -5,20 +5,27 @@
   # $ nix search wget
 
   environment.systemPackages = with pkgs; [
+      #url-bot-rs
       file
       firefox
+      freecad
       gnome3.gnome-tweaks
       google-chrome
       htop
+      inkscape
       kicad
+      nixos-shell
       platformio
       screen
       shotwell
+      simplescreenrecorder
       solvespace
       spotify
+      tilix
+      tor-browser-bundle-bin
+      transmission
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
-      nixos-shell
-      url-bot-rs
+      zoom-us
   ];
 }
