b4d64fb3ef424e809fa61907f05ee699983715c6
Ed Cragg <ed.cragg@eipi.xyz>
2022-05-11 13:51:28 +0100

    url-bot-rs stuff

--- dinky/flake.nix
+++ dinky/flake.nix
@@ -2,32 +2,28 @@
   description = "NixOS configuration";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs?rev=bedc4e5192fc213fc959639a3e636bf29f6b010c";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs?rev=bedc4e5192fc213fc959639a3e636bf29f6b010c";
     nixpkgs-url-bot.url = "github:nixos/nixpkgs?rev=4e5f0ca4b33180243f2c6e9bd25951ce3ed4a1b6";
+
+    url-bot.url = github:nuxeh/url-bot-rs/flake-test;
+
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { home-manager, nixpkgs, nixpkgs-url-bot, ... }@inputs: {
-
-#    packages.x86_64-linux = let
-#      pkgs = import nixpkgs {
-#        system = "x86_64-linux";
-#        config.allowUnfree = true;
-#      }; in {
-#        url-bot-rs = pkgs.callPackage nixpkgs-url-bot.legacyPackages.x86_64-linux.url-bot-rs {};
-#      };
-
+  outputs = { home-manager, nixpkgs, url-bot, ... }@inputs: {
     nixosConfigurations = {
       "dinky-nix" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          #{ nixpkgs.overlays = [ url-bot.overlay ]; }
+
           #(import ./modules/url-bot-rs.nix)
 
-          #nixpkgs-url-bot.modules.url-bot-rs
           #nixpkgs.lib.nixosSystem.modules.vmConfig
 
           ./configuration.nix
+
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
